76b07c389f0f053136f8da0dcb0dbd1783fe5785
wlorin <wlorin@student.ethz.ch>
2014-04-17 11:14:14 +0200

    fix singleton in test 11 and major simplification of codebase in CodeGenerator (most stuff was just duplicated)

--- edu/mit/csail/sdg/alloy4compiler/generator/CodeGenerator.java
+++ edu/mit/csail/sdg/alloy4compiler/generator/CodeGenerator.java
@@ -17,195 +17,149 @@ import edu.mit.csail.sdg.alloy4compiler.generator.VisitorFunc.Argument;
 
 
 public final class CodeGenerator {
-
-  private CodeGenerator(Iterable<Sig> sigs, SafeList<Func> funcs, String originalFilename, PrintWriter out, boolean checkContracts) throws Err {
+	private CodeGenerator(Iterable<Sig> sigs, SafeList<Func> funcs, String originalFilename, PrintWriter out, boolean checkContracts) throws Err {
 	  
-	  Visitor visitor = new Visitor(out);
-	  VisitorFunc visitorFunc = new VisitorFunc(out);
-	  out.println("// This C# file is generated from .." + originalFilename);
-	  out.println();
-	  if (!checkContracts) {
-		  out.println("#undef CONTRACTS_FULL");
-	  }
-	  out.println("using System;\nusing System.Linq;\nusing System.Collections.Generic;\nusing System.Diagnostics.Contracts;");
-	  out.println();
-	  
-	  for (Sig s : sigs){
-		  if (!s.builtin){
-			  //here we first print all classes and then visit each field.
-			  if (s.isTopLevel()){
-				  if (s.isAbstract != null){
-					  out.println("abstract public class " + s.toString().substring(5)+ " {");
-					  try {
-							for (Sig.Field f : s.getFields()) {
-								f.accept(visitor);
-							}
-						} catch (Exception Err) {}
-				  }
-				  else {
-				  out.println("public class " + s.toString().substring(5)+ " {");
-				  
-					try {
-						for (Field f : s.getFields()) {
-							f.accept(visitor);
-						}
-						
-						//Invariants 
-						if (!s.getFields().isEmpty()){
-							out.println("\n\t[ContractInvariantMethod]");
-							out.println("\tprivate void ObjectInvariant() {");
-							List<Field> crossPFields = new ArrayList<Field>();
-							for (Field f : s.getFields()){
-								String fName = f.label;
-								if (f.type().arity() > 2){
-									String [] t = f.decl().expr.toString().split("this");
-									if (t.length > 3){
-										crossPFields.add(f);
-										out.print("\t\tContract.Invariant(");
-										out.print(fName);
-										out.println(" != null);");
-									}else{
-										out.println("\t\tContract.Invariant(" + fName + " != null && Contract.ForAll(" + fName + ", e1 => e1.Item1 != null && e1.Item2 != null));");
-									}
-								}else if (!f.decl().expr.toString().startsWith("lone ")){
-									//lone fields can be null
-									out.print("\t\tContract.Invariant(");
-									out.print(fName);
-									out.println(" != null);");
-									if (f.decl().expr.setOf().toString().substring(0, 10).contains("set")){ // its a set
-										out.println("\t\tContract.Invariant(Contract.ForAll(" + fName + " != null));"); //TODO I hope this is how it should look
-									}
-									
-								}
-							}
-							for (Field f : crossPFields){
-								String cpName = f.label;
-								String str =  f.decl().expr.toString();
-								out.println(str);
-								String [] lines = str.split("this . \\(this/| <: | |\\) -> |\\)");
-								String fName1 = lines[2];//floor
-								String fName2 = lines[5];//ceiling
-								out.println("\t\tContract.Invariant(" + cpName + " != null && " + fName1 + " != null && Contract.ForAll(" + cpName + ", e1 => " + 
-										 fName1 + ".Equals(e1.Item1)));");
-								out.println("\t\tContract.Invariant(" + cpName + " != null && " + fName2 + " != null && Contract.ForAll(" + cpName + ", e1 => " + 
-										 fName2 + ".Equals(e1.Item2)));");
-							}
-							out.println("\t}");
-						}
-					} catch (Exception Err) {}
-				  }
-			  }
-			  else { // this sig inherits and has subnodes.
-				 
-				  out.println("public class "+ s.toString().substring(5)+ " : " +s.getSubnodes().get(0).getHTML().substring(24) + " {");
-				  try {
-						for (Field f : s.getFields()) {
-							f.accept(visitor);
-						}
-					} catch (Exception Err) {}
-					finally {}	
-				  if (s.isOne != null){//singleton
-					  String name = s.toString().substring(5);
-					  out.println("\tprivate static " + name + " instance;");
-					  out.println("\tprivate " + name + "() {}");
-					  out.println("\tpublic static " + name + " Instance {");
-					  out.println("\t\tget {");
-					  out.println("\t\t\tif (instance == null) {");
-					  out.println("\t\t\t\tinstance = new " + name + "();");
-					  out.println("\t\t\t}");
-					  out.println("\t\treturn instance;");
-					  out.println("\t\t}");
-					  out.println("\t}");
-				  }
+		Visitor visitor = new Visitor(out);
+		VisitorFunc visitorFunc = new VisitorFunc(out);
+		out.println("// This C# file is generated from .." + originalFilename);
+		out.println();
+		if (!checkContracts) {
+			out.println("#undef CONTRACTS_FULL");
+		}
+		out.println("using System;\nusing System.Linq;\nusing System.Collections.Generic;\nusing System.Diagnostics.Contracts;");
+		out.println();
 
-				//Invariants 
-					if (!s.getFields().isEmpty()){
-						out.println("\n\t[ContractInvariantMethod]");
-						out.println("\tprivate void ObjectInvariant() {");
-						List<Field> crossPFields = new ArrayList<Field>();
-						for (Field f : s.getFields()){
-							String fName = f.label;
-							if (f.type().arity() > 2){
-								String [] t = f.decl().expr.toString().split("this");
-								if (t.length > 3){
-									crossPFields.add(f);
-									out.print("\t\tContract.Invariant(");
-									out.print(fName);
-									out.println(" != null);");
-								}else{
-									out.println("\t\tContract.Invariant(" + fName + " != null && Contract.ForAll(" + fName + ", e1 => e1.Item1 != null && e1.Item2 != null));");
-								}
-							}else{
-								if (f.decl().expr.setOf().toString().substring(0, 10).contains("set")){ // its a set
-									out.println("\t\tContract.Invariant(Contract.ForAll(" + fName + " != null));"); // I hope this is how it should look
-								}else{
-									out.print("\t\tContract.Invariant(");
-									out.print(fName);
-									out.println(" != null);");
-								}
-							}
-						}
-						for (Field f : crossPFields){
-							String cpName = f.label;
-							String str =  f.decl().expr.toString();
-							String [] lines = str.split("this . \\(this/| <: | |\\) -> |\\)");
-							String fName1 = lines[2];//floor
-							String fName2 = lines[5];//ceiling
-							out.println("\t\tContract.Invariant(" + cpName + " != null && " + fName1 + " != null && Contract.ForAll(" + cpName + ", e1 => " + 
-									 fName1 + ".Equals(e1.Item1)));");
-							out.println("\t\tContract.Invariant(" + cpName + " != null && " + fName2 + " != null && Contract.ForAll(" + cpName + ", e1 => " + 
-									 fName2 + ".Equals(e1.Item2)));");
-						}
-						out.println("\t}");
+		for (Sig s : sigs) {
+			if (!s.builtin) {
+				// here we first print all classes and then visit each field.
+				if (s.isAbstract != null) {
+					out.print("abstract ");
+				}
+				if (s.isTopLevel()) {
+					out.println("public class " + s.toString().substring(5)
+							+ " {");
+				} else {
+					out.println("public class " + s.toString().substring(5)
+							+ " : "
+							+ s.getSubnodes().get(0).getHTML().substring(24)
+							+ " {");
+				}
+				try {
+					for (Sig.Field f : s.getFields()) {
+						f.accept(visitor);
 					}
-				  
-				  
-			  }
-			   
-			out.println("}");
-			out.println();
-		  }
-	  }
-	  
-	  
-	  
-	  
-	  
-	// functions
-		  String closeBracket = "";
-		  if (funcs.size() > 0) {
-			  out.println("public static class FuncClass {");
-			  closeBracket = "}";
-		  }
-		  for (Func f : funcs) {
-			  out.print("\tpublic static ");
-			  String returnType = visitorFunc.parseReturnType(f); 
-			  List<Argument> args = visitorFunc.parseFuncParams(f);
-			  out.print(returnType);
-			  out.print(" ");
-			  out.print(f.label.substring(5));
-			  out.print("(");
-			  out.print(visitorFunc.joinArgumentList(args));
-			  out.println(") {");
-			  if (f.isPred) {
-				  //predicates
-				  out.print("\t\treturn ");
-				  f.getBody().accept(visitorFunc);
-				  out.println(";\n\t}");
-			  }
-			  else {
-				  out.print("\t\t" + visitorFunc.argumentsNotNullContracts(args));
-				  out.print("\t\t" + visitorFunc.returnValueNotNullContract(returnType));
-				  out.print("\n");
-				  f.getBody().accept(visitorFunc);
-				  out.println("\n\t}");
-			  }
-		  }
-		  
-		  out.println(closeBracket);
-		  out.println(visitorFunc.Closurefunction);
-		  out.close();
-  }
+				} catch (Exception Err) {
+				}
+				if (s.isOne != null) {// singleton
+					String name = s.toString().substring(5);
+					out.println("\tprivate static " + name + " instance;");
+					out.println("\tprivate " + name + "() {}");
+					out.println("\tpublic static " + name + " Instance {");
+					out.println("\t\tget {");
+					out.println("\t\t\tif (instance == null) {");
+					out.println("\t\t\t\tinstance = new " + name + "();");
+					out.println("\t\t\t}");
+					out.println("\t\treturn instance;");
+					out.println("\t\t}");
+					out.println("\t}");
+				}
+
+				// Invariants
+				if (!s.getFields().isEmpty()) {
+					out.println("\n\t[ContractInvariantMethod]");
+					out.println("\tprivate void ObjectInvariant() {");
+					List<Field> crossPFields = new ArrayList<Field>();
+					for (Field f : s.getFields()) {
+						String fName = f.label;
+						if (f.type().arity() > 2) {
+							String[] t = f.decl().expr.toString().split("this");
+							if (t.length > 3) {
+								crossPFields.add(f);
+								out.print("\t\tContract.Invariant(");
+								out.print(fName);
+								out.println(" != null);");
+							} else {
+								out.println("\t\tContract.Invariant("
+										+ fName
+										+ " != null && Contract.ForAll("
+										+ fName
+										+ ", e1 => e1.Item1 != null && e1.Item2 != null));");
+							}
+						} else if (!f.decl().expr.toString()
+								.startsWith("lone ")) {
+							// lone fields can be null
+							out.print("\t\tContract.Invariant(");
+							out.print(fName);
+							out.println(" != null);");
+							if (f.decl().expr.setOf().toString()
+									.substring(0, 10).contains("set")) { // its a set
+								out.println("\t\tContract.Invariant(Contract.ForAll(" + fName + " != null));"); // TODO I hope this is how it shouldlook
+							}
+
+						}
+					}
+					for (Field f : crossPFields) {
+						String cpName = f.label;
+						String str = f.decl().expr.toString();
+						String[] lines = str.split("this . \\(this/| <: | |\\) -> |\\)");
+						String fName1 = lines[2];// floor
+						String fName2 = lines[5];// ceiling
+						out.println("\t\tContract.Invariant(" + cpName
+								+ " != null && " + fName1
+								+ " != null && Contract.ForAll(" + cpName
+								+ ", e1 => " + fName1 + ".Equals(e1.Item1)));");
+						out.println("\t\tContract.Invariant(" + cpName
+								+ " != null && " + fName2
+								+ " != null && Contract.ForAll(" + cpName
+								+ ", e1 => " + fName2 + ".Equals(e1.Item2)));");
+					}
+					out.println("\t}");
+				}
+
+				out.println("}");
+				out.println();
+			}
+		}
+  
+  
+  
+  
+  
+		// functions
+		String closeBracket = "";
+		if (funcs.size() > 0) {
+			out.println("public static class FuncClass {");
+			closeBracket = "}";
+		}
+		for (Func f : funcs) {
+			out.print("\tpublic static ");
+			String returnType = visitorFunc.parseReturnType(f);
+			List<Argument> args = visitorFunc.parseFuncParams(f);
+			out.print(returnType);
+			out.print(" ");
+			out.print(f.label.substring(5));
+			out.print("(");
+			out.print(visitorFunc.joinArgumentList(args));
+			out.println(") {");
+			if (f.isPred) {
+				// predicates
+				out.print("\t\treturn ");
+				f.getBody().accept(visitorFunc);
+				out.println(";\n\t}");
+			} else {
+				out.print("\t\t" + visitorFunc.argumentsNotNullContracts(args));
+				out.print("\t\t"
+						+ visitorFunc.returnValueNotNullContract(returnType));
+				out.print("\n");
+				f.getBody().accept(visitorFunc);
+				out.println("\n\t}");
+			}
+		}
+
+		out.println(closeBracket);
+		out.println(visitorFunc.Closurefunction);
+		out.close();
+	}
 
   public static void writeCode(Module world, String originalFilename, boolean checkContracts, boolean saveInDist) throws Err {
     try {
